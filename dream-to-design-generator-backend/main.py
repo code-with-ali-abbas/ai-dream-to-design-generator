cbd1bb0dfc085a1fb1169ca33259dd7d900e64f5
Ali Abbas <aliabbasofficial726@gmail.com>
2025-07-31 09:27:45 -0400

    added backend implementation

---
+++ dream-to-design-generator-backend/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from diffusers import StableDiffusionPipeline
+import torch
+import uuid
+import os
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# CORS for frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class PromptRequest(BaseModel):
+  prompt: str
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch_dtype
+)
+
+pipe = pipe.to(device)
+
+@app.post("/generate-image")
+
+async def generate_image(request: PromptRequest):
+  image = pipe(request.prompt).images[0]
+
+  filename = f"{uuid.uuid4().hex}.png"
+  image_path = f"static/{filename}"
+  os.makedirs("static", exist_ok=True)
+  image.save(image_path)
+  return {"url": f"http://localhost:8000/{image_path}"}
